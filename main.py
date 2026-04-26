3e079c3cb017f74af403640101556cfebc22dda8
bellodigital <abuafradigital@gmail.com>
2026-04-26 01:33:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -573,5 +573,5 @@ if __name__ == "__main__":
     port = int(os.getenv("PORT", "8080"))
     logger.info(f"Flask on port {port}")
     app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
-```
+
 
